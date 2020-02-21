6e0dd1175c3320e2d453019df379d7e66fbe97a7
theoricien <agressou@gmail.com>
2020-02-21 06:28:46 +0100

    #23 Creation de la classe FileManager
    FileManager est une classe permettant la gestion simplifiée des fichiers pour notre application.
    Elle permet d'écrire dans un fichier, de lire un fichier, de savoir s'il existe et de le vider si besoin est.

---
+++ javastd/serveur/src/main/java/serveur/FileManager.java
@@ -0,0 +1,125 @@
+package serveur;
+
+import java.io.*;
+
+/**
+ * Permet de gerer avec plus de facilite les fichiers de l'application
+ * @author theoricien
+ */
+public class 
+FileManager 
+{
+	/* FIELDS */
+	/**
+	 * file represente un fichier sous un File
+	 */
+	private File file;
+	
+	/* CONSTRUCTORS */
+	/**
+	 * Constructeur a l'aide d'une String
+	 * @param file une String representant le nom du fichier voulu
+	 */
+	public
+	FileManager (String file)
+	{this.file = new File(file);}
+	
+	/**
+	 * Constructeur avec un File
+	 * @param file un File
+	 */
+	public
+	FileManager (File file)
+	{this.file = file;}
+	
+	/* METHODS */
+	/**
+	 * Permet de vider totalement un fichier
+	 */
+	public void
+	clearFile ()
+	{this.write("");}
+	
+	/**
+	 * Permet de savoir si le fichier existe ou non
+	 * @return Un boolean
+	 */
+	public boolean
+	exists ()
+	{return this.file.exists();}
+	
+	/**
+	 * Permet de remplacer le contenu d'un fichier par content
+	 * @param content Le futur contenu du fichier
+	 */
+	public void
+    write (String content)
+	{
+		// Si on a les droits pour ecrire
+		if (this.file.canWrite())
+		{
+			PrintWriter pw;
+			// On essaye d'ecrire
+			try {
+				pw = new PrintWriter(this.file);
+				// On essaye d'ecrire ici
+				pw.write(content);
+				pw.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				Debug.error("File " + this.file.getName() + " does not exist !");
+				System.exit(2);
+			}
+		} else {
+			Debug.error(this.file.getName() + " isn\'t writable !");
+			System.exit(2);
+		}
+
+	}
+	
+	/**
+	 * Permet de recuperer la premiere ligne de notre fichier, puisque dans notre
+	 * application, il n'y a qu'1 seule ligne par fichier
+	 * @return Une String
+	 */
+	public String
+	getFileContent ()
+	{
+		// Si on a les droits pour lire le fichier
+		if (this.file.canRead())
+		{
+			// On essaye de lire la premiere ligne
+			try
+			{
+				/* INITIALISATION */
+				FileReader r = new FileReader(this.file);
+				BufferedReader br = new BufferedReader(r);
+				/* GET THE CONTENT */
+				String tmp = br.readLine();
+				/* CLOSE FILESTREAMS */
+				br.close();
+				r.close();
+				
+				// On renvoie la premier ligne
+				return tmp;
+			} catch (Exception e) {
+				e.printStackTrace();
+				Debug.error(this.file.getName() + " doesn\'t exist !");
+			}
+		} else {
+			Debug.error(this.file.getName() + " isn\'t readable !");
+			System.exit(2);
+		}
+		return "";
+	}
+	
+	/* GETTER */
+	/**
+	 * Getter qui renvoie le File file pour des manipulations plus precises.
+	 * file represente un fichier sous un File
+	 * @return un File
+	 */
+	public File
+	getFile ()
+	{return this.file;}
+}
