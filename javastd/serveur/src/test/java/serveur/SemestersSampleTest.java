f1a50d855c72bc39bbfd4dee72958b33d68d2405
Yann Martin D'Escrienne <yann.martindes@orange.fr>
2020-03-05 21:10:02 +0100

    Update SemestersSampleTest.java

--- javastd/serveur/src/test/java/serveur/SemestersSampleTest.java
+++ javastd/serveur/src/test/java/serveur/SemestersSampleTest.java
@@ -9,29 +9,34 @@ public class SemestersSampleTest {
     @Test
     public void s1Test(){
         String res = SemestersSample.S1Jsoned;
-        String expected = "{\"number\":1,\"listCategorie\":[{\"name\":\"GEOGRAPHIE\"," +
-                "\"listUE\":[{\"name\":\"Decouverte 1\",\"code\":\"SPUGDE10\",\"checked\":false}," +
-                "{\"name\":\"Decouverte 2\",\"code\":\"SPUGDC10\",\"checked\":false}," +
-                "{\"name\":\"Decouverte 1\",\"code\":\"SPUGDI10\",\"checked\":false}]}," +
+        String expected = "{\"number\":1,\"listCategorie\":[{\"name\":\"GEOGRAPHIE\",\"listUE\":" +
+                "[{\"name\":\"Decouverte 1\",\"code\":\"SPUGDE10\",\"categorie\":\"GEOGRAPHIE\"}," +
+                "{\"name\":\"Decouverte 2\",\"code\":\"SPUGDC10\",\"categorie\":\"GEOGRAPHIE\"}," +
+                "{\"name\":\"Decouverte 1\",\"code\":\"SPUGDI10\",\"categorie\":\"GEOGRAPHIE\"}]}," +
                 "{\"name\":\"INFORMATIQUE\",\"listUE\":[{\"name\":\"Bases de l\\u0027informatique\"," +
-                "\"code\":\"SPUF10\",\"checked\":false},{\"name\":\"Introduction a l\\u0027informatique par le web\"," +
-                "\"code\":\"SPUF11\",\"checked\":false}]},{\"name\":\"MATHEMATIQUES\",\"listUE\":[{\"name\":\"Fondements 1\"," +
-                "\"code\":\"SPUM11\",\"checked\":false},{\"name\":\"Complements 1\",\"code\":\"SPUM13\",\"checked\":false}," +
-                "{\"name\":\"Methodes - approche continue\",\"code\":\"SPUM12\",\"checked\":false}]}," +
-                "{\"name\":\"SCIENCES DE LA VIE\",\"listUE\":[{\"name\":\"Genetique. evolution. origine vie \\u0026 biodiversite\"," +
-                "\"code\":\"SPUV101\",\"checked\":false},{\"name\":\"Org et mecan. moleculaires - cellules eucaryotes\"," +
-                "\"code\":\"SPUV100\",\"checked\":false},{\"name\":\"Structure microscopique de la Categorie\"," +
-                "\"code\":\"SPUC10\",\"checked\":false}]},{\"name\":\"ELECTRONIQUE\"," +
-                "\"listUE\":[{\"name\":\"Electronique numerique - Bases\",\"code\":\"SPUE10\",\"checked\":false}]}," +
-                "{\"name\":\"ECONOMIE - GESTION\",\"listUE\":[{\"name\":\"Economie-gestion\",\"code\":\"SPUA10\",\"checked\":false}]}," +
-                "{\"name\":\"PHYSIQUE\",\"listUE\":[{\"name\":\"Mecanique 1\",\"code\":\"SPUP10\",\"checked\":false}]}," +
-                "{\"name\":\"SCIENCES DE LA TERRE\",\"listUE\":[{\"name\":\"Decouverte des sciences de la terre\"," +
-                "\"code\":\"SPUT10\",\"checked\":false}]},{\"name\":\"MATH ENJEUX\",\"listUE\":[{\"name\":\"Math enjeux 1\"," +
-                "\"code\":\"SPUS10\",\"checked\":false}]},{\"name\":\"COMPETENCES TRANSVERSALE\"," +
-                "\"listUE\":[{\"name\":\"Competences transversales\",\"code\":\"KCTTS1\",\"checked\":false}]}," +
-                "{\"name\":\"FABLAB\",\"listUE\":[{\"name\":\"Fablab S1\",\"code\":\"SPUSF100\",\"checked\":false}]}]}";
+                "\"code\":\"SPUF10\",\"categorie\":\"INFORMATIQUE\"},{\"name\":\"Introduction a" +
+                " l\\u0027informatique par le web\",\"code\":\"SPUF11\",\"categorie\":\"INFORMATIQUE\"}]}," +
+                "{\"name\":\"MATHEMATIQUES\",\"listUE\":[{\"name\":\"Fondements 1\",\"code\":" +
+                "\"SPUM11\",\"categorie\":\"MATHEMATIQUES\"},{\"name\":\"Complements 1\",\"code\":\"SPUM13\"," +
+                "\"categorie\":\"MATHEMATIQUES\"},{\"name\":\"Methodes - approche continue\",\"code\":\"SPUM12\"," +
+                "\"categorie\":\"MATHEMATIQUES\"}]},{\"name\":\"SCIENCES DE LA VIE\",\"listUE\":[{\"name\":" +
+                "\"Genetique. evolution. origine vie \\u0026 biodiversite\",\"code\":\"SPUV101\",\"categorie\":" +
+                "\"SCIENCES DE LA VIE\"},{\"name\":\"Org et mecan. moleculaires - cellules eucaryotes\",\"code\":" +
+                "\"SPUV100\",\"categorie\":\"SCIENCES DE LA VIE\"},{\"name\":\"Structure microscopique de la matiere\"," +
+                "\"code\":\"SPUC10\",\"categorie\":\"SCIENCES DE LA VIE\"}]},{\"name\":\"ELECTRONIQUE\",\"listUE\":" +
+                "[{\"name\":\"Electronique numerique - Bases\",\"code\":\"SPUE10\",\"categorie\":\"ELECTRONIQUE\"}]}," +
+                "{\"name\":\"ECONOMIE - GESTION\",\"listUE\":[{\"name\":\"Economie-gestion\",\"code\":\"SPUA10\",\"" +
+                "categorie\":\"ECONOMIE - GESTION\"}]},{\"name\":\"PHYSIQUE\",\"listUE\":[{\"name\":\"Mecanique 1\"," +
+                "\"code\":\"SPUP10\",\"categorie\":\"PHYSIQUE\"}]},{\"name\":\"SCIENCES DE LA TERRE\",\"listUE\":[{" +
+                "\"name\":\"Decouverte des sciences de la terre\",\"code\":\"SPUT10\",\"categorie\":\"SCIENCES DE LA " +
+                "TERRE\"}]},{\"name\":\"MATH ENJEUX\",\"listUE\":[{\"name\":\"Math enjeux 1\",\"code\":\"SPUS10\",\"" +
+                "categorie\":\"MATH ENJEUX\"}]},{\"name\":\"COMPETENCES TRANSVERSALES\",\"listUE\":[{\"name\":\"" +
+                "Competences transversales\",\"code\":\"KCTTS1\",\"categorie\":\"COMPETENCES TRANSVERSALES\"}]}," +
+                "{\"name\":\"FABLAB\",\"listUE\":[{\"name\":\"Fablab S1\",\"code\":\"SPUSF100\",\"categorie\":" +
+                "\"FABLAB\"}]}],\"numberUENeedChoose\":5,\"maxNumberByCategorie\":1,\"listObligatory\":[\"SPUM11\"," +
+                "\"SPUM12\"]}";
 
 
-        assertEquals(res,expected);
+        assertEquals(expected,res);
     }
 }
