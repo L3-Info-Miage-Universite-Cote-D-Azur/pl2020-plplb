68301cd19a0293c7f345ec8e823f2ff47bdaacab
theoricien <agressou@gmail.com>
2020-03-19 21:57:40 +0100

    Rendu plus user-friendly dans la console du serveur

--- javastd/serveur/src/main/java/App.java
+++ javastd/serveur/src/main/java/App.java
@@ -9,7 +9,7 @@ public class App {
 
     public static final void main(String[] args) {
 
-        String ip = "0.0.0.0";
+        String ip = "127.0.0.1";
         int port = 10101;
                 
         Serveur serv = new Serveur(ip, port);
