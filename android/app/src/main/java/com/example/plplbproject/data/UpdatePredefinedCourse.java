32d715de887756579b167c265b193b2ead585f3e
Yann Martin D'Escrienne <yann.martindes@orange.fr>
2020-04-01 16:00:45 +0200

    #141 changement de la reception du json

--- android/app/src/main/java/com/example/plplbproject/data/UpdatePredefinedCourse.java
+++ android/app/src/main/java/com/example/plplbproject/data/UpdatePredefinedCourse.java
@@ -12,7 +12,13 @@ public class UpdatePredefinedCourse implements Emitter.Listener {
     @Override
     public void call(Object... args) {
         Gson gson = new Gson();
-        ArrayList<ParcoursType> predefinedCourseList = gson.fromJson((String) args[0], ArrayList.class);
+        ArrayList<ParcoursType> predefinedCourseList = new ArrayList<ParcoursType>();
+        ArrayList<String> data = gson.fromJson((String) args[0], ArrayList.class);
+
+        //On recupere une liste de json de parcoursType
+        for(String s : data){
+            predefinedCourseList.add(gson.fromJson(s,ParcoursType.class));
+        }
 
         DataPredefinedCourse.PREDEFINEDCOURSE.setPredefinedCourseList(predefinedCourseList);
     }
