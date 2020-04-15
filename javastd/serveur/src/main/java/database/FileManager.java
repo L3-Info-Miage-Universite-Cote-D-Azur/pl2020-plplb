07f9e6e3a1dc6a0ff901d4b458be7b1ae6280745
Idrash <56682531+Idrash@users.noreply.github.com>
2020-04-15 15:17:03 +0200

    Ajout de fonction FileManager pour #169

--- javastd/serveur/src/main/java/database/FileManager.java
+++ javastd/serveur/src/main/java/database/FileManager.java
@@ -1,181 +1,174 @@
-package database;
-
-import java.io.*;
+package dataBase;
 
 import debug.Debug;
 
-/**
- * Permet de gerer avec plus de facilite les fichiers de l'application
- * @author theoricien
- */
-public class 
-FileManager 
-{
-	/* FIELDS */
-	/**
-	 * file represente un fichier sous un File
-	 */
-	private File file;
-	
-	/* CONSTRUCTORS */
-	/**
-	 * Constructeur a l'aide d'une String
-	 * @param file une String representant le nom du fichier voulu
-	 */
-	public
-	FileManager (String file)
-	{this.file = new File(file);}
-	
-	/**
-	 * Constructeur avec un File
-	 * @param file un File
-	 */
-	public
-	FileManager (File file)
-	{this.file = file;}
-	
-	/* METHODS */
-	/**
-	 * Permet de vider totalement un fichier
-	 */
-	public void
-	clearFile ()
-	{this.write("");}
-	
-	/**
-	 * Permet de creer le fichier
-	 */
-	public void
-	create ()
-	{
-		try {
-			this.file.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Permet de savoir si le fichier existe ou non
-	 * @return Un boolean
-	 */
-	public boolean
-	exists ()
-	{return this.file.exists();}
-	
-	/**
-	 * Permet de remplacer le contenu d'un fichier par content
-	 * @param content Le futur contenu du fichier
-	 */
-	public void
-    write (String content)
-	{
-		// Si on a les droits pour ecrire
-		if (this.file.canWrite())
-		{
-			PrintWriter pw;
-			// On essaye d'ecrire
-			try {
-				pw = new PrintWriter(this.file);
-				// On essaye d'ecrire ici
-				pw.write(content);
-				pw.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-				Debug.error("File " + this.file.getName() + " does not exist !");
-			}
-		} else {
-			Debug.error(this.file.getName() + " isn\'t writable !");
-		}
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-	}
-	
-	/**
-	 * Permet de recuperer la premiere ligne de notre fichier, puisque dans notre
-	 * application, il n'y a qu'1 seule ligne par fichier
-	 * @return Une String
-	 */
-	public String
-	getFileContent ()
-	{
-		// Si on a les droits pour lire le fichier
-		if (this.file.canRead())
-		{
-			// On essaye de lire la premiere ligne
-			try
-			{
-				/* INITIALISATION */
-				FileReader r = new FileReader(this.file);
-				BufferedReader br = new BufferedReader(r);
-				/* GET THE CONTENT */
-				String tmp = br.readLine();
-				/* CLOSE FILESTREAMS */
-				br.close();
-				r.close();
-				
-				// On renvoie la premier ligne
-				return tmp;
-			} catch (Exception e) {
-				e.printStackTrace();
-				Debug.error(this.file.getName() + " doesn\'t exist !");
-			}
-		} else {
-			Debug.error(this.file.getName() + " isn\'t readable !");
-		}
-		return "";
-	}
-	
-	/**
-	 * Permet de renvoyer tout le contenu d'un fichier
-	 * sous forme brut
-	 * @return un String
-	 */
-	public String
-	getRaw ()
-	{
-		// Si on a les droits pour lire le fichier
-		if (this.file.canRead())
-		{
-			// On essaye de lire la premiere ligne
-			try
-			{
-				/* INITIALISATION */
-				FileReader r = new FileReader(this.file);
-				BufferedReader br = new BufferedReader(r);
-				StringBuilder sb = new StringBuilder();
-				/* GET THE CONTENT */
-				String tmp;
-				//On remet le \n sauf la premiere fois que l'on lit une ligne
-				if((tmp = br.readLine()) != null){
-					sb.append(tmp);
-				}
-				while ((tmp = br.readLine()) != null)
-				{
-					sb.append("\n"+tmp);
-				}
-				/* CLOSE FILESTREAMS */
-				br.close();
-				r.close();
-				
-				// On renvoie la premier ligne
-				return sb.toString();
-			} catch (Exception e) {
-				e.printStackTrace();
-				Debug.error(this.file.getName() + " doesn\'t exist !");
-			}
-		} else {
-			Debug.error(this.file.getName() + " isn\'t readable !");
-		}
-		return "";
-	}
-	
-	/* GETTER */
-	/**
-	 * Getter qui renvoie le File file pour des manipulations plus precises.
-	 * file represente un fichier sous un File
-	 * @return un File
-	 */
-	public File
-	getFile ()
-	{return this.file;}
+public class FileManager {
+    /* FIELDS */
+    /**
+     * file represente un fichier que le fileManager s'occupe
+     */
+    private File file;
+
+    /**
+     * Constructeur a l'aide d'une String
+     * @param path une String representant le chemin du fichier voulue
+     */
+    public FileManager (String path)
+    {this.file = new File(path);}
+
+    /**
+     * Constructeur a l'aide d'un file
+     * @param file le file qui vas etre utiliser
+     */
+    public FileManager (File file)
+    {this.file = file;}
+
+
+    /**
+     * Permet de créer plusieur plusieur fileManager a
+     * l'aide d'un chemin de de nom de fichier
+     * @param path le repertoir a partir du quel sont stocker/créer les fichiers
+     * @param filesName la liste des nom de fichier
+     * @return une list de fileManager
+     */
+    public static List<FileManager> ListToFileManager(String path , List<String> filesName) {
+        ArrayList<FileManager> filesManager = new ArrayList<FileManager>();
+        for (String name : filesName) {
+            filesManager.add(new FileManager(new File(path,name)));
+        }
+        return filesManager;
+    }
+
+
+    /**
+     * Permet de creer le fichier
+     */
+    public boolean create()
+    {
+        try {
+            this.file.createNewFile();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Debug.error(this.file.getName()+ "can\'t be create");
+        }
+        return false;
+    }
+
+    /**
+     * Permet de savoir si le fichier existe ou non
+     * @return Un boolean
+     */
+    public boolean exists ()
+    {return this.file.exists();}
+
+    /**
+     * Permet de remplacer le contenu d'un fichier par content
+     * @param content Le futur contenu du fichier
+     */
+    public void write (String content)
+    {
+        //si le fichier n'est pas encore créer on le crée
+        if (!file.exists()) {
+            boolean isCreate = create();
+            if(!isCreate) return;
+        }
+
+        // Si on a les droits pour ecrire
+        if(file.canWrite()){
+            PrintWriter pw;
+            // On essaye d'ecrire
+            try {
+                pw = new PrintWriter(this.file);
+                // On essaye d'ecrire ici
+                pw.write(content);
+                pw.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Debug.error("File " + this.file.getName() + " does not exist !");
+            }
+
+        } else {
+            Debug.error(this.file.getName() + " isn\'t writable !");
+        }
+
+    }
+
+    /**
+     * Permet de renvoyer tout le contenu d'un fichier
+     * sous forme brut
+     * @return un String
+     */
+    public String getRaw()
+    {
+
+        //si le fichier n'est pas créer
+        if (!file.exists()) return null;
+
+        // Si on a les droits pour lire le fichier
+        if (this.file.canRead())
+        {
+            // On essaye de lire la premiere ligne
+            try
+            {
+                /* INITIALISATION */
+                FileReader r = new FileReader(this.file);
+                BufferedReader br = new BufferedReader(r);
+                StringBuilder resultString = new StringBuilder();
+                /* GET THE CONTENT */
+                String tmp;
+                //On remet le \n sauf la premiere fois que l'on lit une ligne
+                if((tmp = br.readLine()) != null){
+                    resultString.append(tmp);
+                }
+                while ((tmp = br.readLine()) != null)
+                {
+                    resultString.append("\n"+tmp);
+                }
+                /* CLOSE FILESTREAMS */
+                br.close();
+                r.close();
+
+                // On renvoie la premier ligne
+                return resultString.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Debug.error(this.file.getName() + " doesn\'t exist !");
+            }
+        } else {
+            Debug.error(this.file.getName() + " isn\'t readable !");
+        }
+        return "";
+    }
+
+    /**
+     * Permet de suprimer un fichier
+     */
+    public void deleteFile(){
+        if(exists()) {
+            file.delete();
+        }
+    }
+
+    /**
+     * Permet de recueperer le chemin du repertoire parent
+     * @return le chemin du repertoire parent
+     */
+    public String getParentPath(){
+        return file.getParent();
+    }
+
+
+
+
+
+    /**
+     * Permet de vider totalement un fichier
+     */
+    public void clearFile ()
+    {write("");}
 }
